d0981fe392e5f454b714b0d0e48afd2192b489b1
ivan3123708 <ivan3123708@gmail.com>
2018-10-18 16:35:33 +0200

    #LVAG-10 added navbar to header partial

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,1 @@
-<div id="footer">Lorem Ipsum &copy; 2018</div>
+<div id="footer" style="text-align: center; margin-top: 100px;">Lorem Ipsum &copy; 2018</div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,23 +1,32 @@
-<div id="header">
-    <div id="logo">
-        <a href="/"><img src="https://fusiongroupusa.com/wp-content/uploads/2017/11/logo.png" /></a>
-    </div>
-    <div>
-        @auth
-            <a href="{{ url('/home') }}">Home</a>
-            <p>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</p>
-            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-laravel">
+    <a class="navbar-brand" href={{ url('/') }}>
+        <img src="https://fusiongroupusa.com/wp-content/uploads/2017/11/logo.png" />
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item dropdown">
+            @auth
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}
             </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-            </form>
-        @else
-            <a href="{{ route('login') }}">Login</a>
-    
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}">Register</a>
-            @endif
-        @endauth
+            @else
+                <a class="nav-link">Geust</a>
+            @endauth
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+        </li>
+        </ul>
     </div>
-</div>
+</nav>
